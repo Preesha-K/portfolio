cd4b337ea2c9855c9e2edd4479be68ea6f9911be
Preesha-K <168195631+Preesha-K@users.noreply.github.com>
2024-09-02 16:49:10 +0530

    Update form scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,3 @@
-/*  ================
-
-Theme Name: Julio
-Theme URI: https://demo.techeshta.com/html/Julio/
-Description: Julio is a creative portfolio template
-Author: Techeshta
-Author URI: https://www.templatemonster.com/authors/techeshta/
-Version: 1.0
-
-=======================  */
 
 /* =======================
 
@@ -140,34 +130,3 @@ $(function() {
         
 }); 
 
-/* Form Submitting */
-var form = document.getElementById("contact-form");
-    
-async function handleSubmit(event) {
-    event.preventDefault();
-    var status = document.getElementById("contact-form-status");
-    var data = new FormData(event.target);
-    fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            status.innerHTML = "Thanks for your submission!";
-            form.reset()
-        } else {
-            response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-                status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-                status.innerHTML = "Oops! There was a problem submitting your form"
-            }
-        })
-    }
-    }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-    });
-}
-form.addEventListener("submit", handleSubmit)
